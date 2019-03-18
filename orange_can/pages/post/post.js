a576621c0f03342693afcb84c48b628f7fb3d6ba
shencp <15858127547@139.com>
2019-03-18 22:22:53 +0800

    js set page jump behavior

--- orange_can/pages/post/post.js
+++ orange_can/pages/post/post.js
@@ -12,24 +12,54 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log('1 ---- onLoad');
 
-    var initData = {
-      object: {
-        date: "2019-3-17"
+    var postList = [{
+        object: {
+          date: "2019-3-17"
+        },
+        title: "小时候的冰棍与雪糕",
+        postImg: "/images/post/post-1.jpg",
+        avatar: "/images/avatar/avatar-1.png",
+        content: "冰棍雪糕绝对不是同一个东西。3毛到5毛的雪糕不亚于现在的哈根达斯，5分到1毛的冰棍，就像现在的老冰棍，时过境迁，...",
+        readingNum: 92,
+        collectionNum: {
+          array: [11]
+        },
+        commentNum: 99
       },
-      title: "小时候的冰棍与雪糕",
-      postImg: "/images/post/post-1.jpg",
-      avatar: "/images/avatar/avatar-1.png",
-      content: "冰棍雪糕绝对不是同一个东西。3毛到5毛的雪糕不亚于现在的哈根达斯，5分到1毛的冰棍，就像现在的老冰棍，时过境迁，...",
-      readingNum: 92,
-      collectionNum: {
-        array: [11]
+      {
+        object: {
+          date: "2019-3-17"
+        },
+        title: "小时候的冰棍与雪糕",
+        postImg: "/images/post/post-2.jpg",
+        avatar: "/images/avatar/avatar-1.png",
+        content: "冰棍雪糕绝对不是同一个东西。3毛到5毛的雪糕不亚于现在的哈根达斯，5分到1毛的冰棍，就像现在的老冰棍，时过境迁，...",
+        readingNum: 92,
+        collectionNum: {
+          array: [11]
+        },
+        commentNum: 99
       },
-      commentNum: 99
-    }
+      {
+        object: {
+          date: "2019-3-17"
+        },
+        title: "小时候的冰棍与雪糕",
+        postImg: "/images/post/post-3.jpg",
+        avatar: "/images/avatar/avatar-1.png",
+        content: "冰棍雪糕绝对不是同一个东西。3毛到5毛的雪糕不亚于现在的哈根达斯，5分到1毛的冰棍，就像现在的老冰棍，时过境迁，...",
+        readingNum: 92,
+        collectionNum: {
+          array: [11]
+        },
+        commentNum: 99
+      }
+
+    ]
+
     this.setData({
-      updateData:initData
+      postList: postList
     })
 
   },
--- orange_can/pages/welcome/welcome.js
+++ orange_can/pages/welcome/welcome.js
@@ -1,5 +1,24 @@
 // pages/welcome/welcome.js
 Page({
+  // ------------------------------------------------------------------------------------------
+
+  onTpJp: function (event) {
+    wx.redirectTo({
+      url: '../post/post',
+      success: function () {
+        console.log('jump success')
+      },
+      fail: function () {
+        console.log('jump fail')
+      },
+      complete: function () {
+        console.log('jump complete')
+      }
+    })
+  },
+
+  // ------------------------------------------------------------------------------------------
+
 
   /**
    * 页面的初始数据
@@ -11,56 +30,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
